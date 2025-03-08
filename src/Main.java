1a121ad5c77d3616f823e848df31a02a9589549e
Mehmet Ali Tezcan <mehmetalitez33@gmail.com>
2025-03-08 21:50:00 +0300

    ilk

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,23 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Bir sayı giriniz: ");
+        int sayi= scanner.nextInt();
+        scanner.close();
+        int bolenlerToplami=0;
+
+        for (int i = 1; i < sayi/2; i++) {
+            if (sayi%i==0){
+                bolenlerToplami+=i;
+            }
         }
+        if (sayi==bolenlerToplami){
+            System.out.println(sayi + " Mükemmel sayıdır.✅");
+        }else System.out.println(sayi + " Mükemmel sayı değildir.❌");
     }
 }
